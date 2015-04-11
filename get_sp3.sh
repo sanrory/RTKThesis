7456558beee375ede7bc2c47ff6db2b4a406dab4
Nathan Adler <nathanadler1710@gmail.com>
2015-04-11 14:26:35 +1000

    Script worked - writing in auto-uncompressing.

--- get_sp3.sh
+++ get_sp3.sh
@@ -3,6 +3,8 @@ HOST='cddis.gsfc.nasa.gov'
 USER='anonymous'
 PASSWD='passwd'
 FILE='igu18396_00.sp3.Z'
+LOCAL_FILE_Z='latestCorrection.sp3.Z'
+LOCAL_FILE = 'latestCorrection.sp3'
 
 ftp -n $HOST <<SCRIPT
 user $USER $PASSWD
@@ -11,3 +13,6 @@ binary
 get $FILE
 quit
 SCRIPT
+cp FILE LOCAL_FILE_Z
+uncompress LOCAL_FILE_Z
+mv LOCAL_FILE corrections/LOCAL_FILE
