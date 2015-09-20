ba10360307838832e5352a4a9a8ce19ff395008e
Nathan Adler <nathanadler1710@gmail.com>
2015-09-20 09:33:29 -0700

    Fixing startup script, moving executable.

--- thesis_config/myStartup.sh
+++ thesis_config/myStartup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo odroid | sudo -S wvdial & disown
 sleep 20
-cd ~/rtklib/RTKLIB-master/app/rtkrcv
-screen -d -m rtkrcv -s -o rtkperfectubuntu2.conf
+cd ~/RTK_thesis/RTKThesis/thesis_config
+screen -d -m ./rtkrcv -s -o rtkperfectubuntu3.conf
