03cfd26b1e6e04e2f45955bd7025851992d9c05c
Nathan Adler <nathanadler1710@gmail.com>
2015-10-06 22:32:54 -0700

    Wrote script to download the latest igs08.atx file, compiled everything and now it all works.

--- thesis_config/myStartup.sh
+++ thesis_config/myStartup.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+HOST='ftp.igs.org'
+USER='anonymous'
+PASSWD='nathan.adler@student.unsw.edu.au'
+FILE='igs08.atx'
+
 echo odroid | sudo -S wvdial & disown
 sleep 20
 cd ~/RTK_thesis/RTKThesis/thesis_config
+
+ftp -n $HOST <<SCRIPT
+user $USER $PASSWD
+cd pub/station/general
+binary
+newer $FILE $FILE
+quit
+SCRIPT
+
 screen -d -m ./rtkrcv -s -o rtkperfectubuntu2.conf
+
