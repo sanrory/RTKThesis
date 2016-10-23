18827c0733fc7de7566c27c4dbc19f7ad50371cb
Nathan Adler <nathanadler1710@gmail.com>
2016-10-23 21:46:11 +1100

    Modified startup scripts and binaries for raspberry pi using UART.

--- thesis_config/myStartup.sh
+++ thesis_config/myStartup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cd ~/RTKThesis/thesis_config
-screen -d -m ./rtkrcv2 -s -o newOpts.conf
+#screen -d -m ./rtkrcv2 -s -o newOpts.conf
+#screen -d -m ./rtkrcv2 -s -o rtkLogging2.conf
+screen -d -m sudo ./rtkrcv2 -s -o GPS_UART.conf
