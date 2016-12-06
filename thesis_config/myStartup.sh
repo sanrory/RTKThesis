a1bb75dd3cab5b9de56460a56760b83335a2c29e
Nathan Adler <nathanadler1710@gmail.com>
2016-12-06 14:31:31 +1100

    New config files.

--- thesis_config/myStartup.sh
+++ thesis_config/myStartup.sh
@@ -3,5 +3,6 @@
 cd ~/RTKThesis/thesis_config
 #screen -d -m ./rtkrcv2 -s -o newOpts.conf
 #screen -d -m ./rtkrcv2 -s -o rtkLogging2.conf
-screen -d -m sudo ./rtkrcv2 -s -o GPS_UART.conf
+#screen -d -m sudo ./rtkrcv2 -s -o GPS_UART.conf
+screen -d -m sudo ./rtkrcv2 -s -o GPS_MASCOT.conf
 #screen -d -m ./rtkrcv2 -s -o UNSW.conf
